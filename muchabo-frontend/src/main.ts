765e38daf63ff3c740e97d757df6f6182671f806
Stefan Hentschel <stefan.hentschel@muenchen.de>
2026-09-11 12:55:16 +0200

    fix(muchabo-frontend): prettier

--- muchabo-frontend/src/main.ts
+++ muchabo-frontend/src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from "vue"
-import App from "./App.vue"
-import "./style.css"
+import { createApp } from "vue";
 
-createApp(App).mount("#app")
+import App from "./App.vue";
+
+import "./style.css";
+
+createApp(App).mount("#app");
